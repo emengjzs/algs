c1f9fd6bc09b9afd075a3e0bc953af050540a6be
Ziseon Jiao <ziseon.jiao@sap.com>
2017-02-15 17:18:49 +0800

    Improve insertion sort with sentinel

--- sorting/sort.h
+++ sorting/sort.h
@@ -34,17 +34,28 @@ namespace algs {
   
   template<typename T, template<typename> class Compare>
   void insertion_sort(std::vector<T>& arr, const Compare<T>& cmp) {
-    for (int i = 0; i < arr.size(); ++ i) {
-      T temp = arr[i];
-      for (int j = i - 1; j >= 0; -- j) {
-        if (cmp(temp, arr[j]) > 0) {
-          arr[j + 1] = arr[j];
-        }
-        else {
-          break;
-        }
-        arr[j] = temp;
+    // Check if it is sorted.
+    bool sorted = true;
+    for (int i = arr.size() - 1; i > 0; -- i) {
+      if (cmp(arr[i], arr[i - 1]) > 0) {
+        std::swap(arr[i], arr[i - 1]);
+        sorted = false;
       }
+    }
+    if (sorted) return;
+    
+    // Original insertion sort, notice the i is begin from 1, not 0!
+    for (int i = 1; i < arr.size(); ++ i) {
+      T temp = arr[i];
+      int j = i - 1;
+      // Don't have to check if j is out of bound.
+      while (cmp(temp, arr[j]) > 0) {
+        arr[j + 1] = arr[j];
+        -- j;
+      }
+      // notice that the index we put temp in is j + 1, not j ! 
+      // When a variable breaks from while/for, the value is already invalid
+      arr[j + 1] = temp;
     }
   }
   
