6da182026077e8b515b950fefb3241186f87ac95
Ziseon Jiao <ziseon.jiao@sap.com>
2017-02-15 17:43:23 +0800

    Rename sort to elementory_sort

--- sorting/elementory_sort.h
+++ sorting/elementory_sort.h
@@ -13,6 +13,8 @@
 #include <vector>
 #include <utility>
 
+#include "compare.h"
+
 namespace emeng {
 namespace algs {
   
@@ -60,31 +62,6 @@ namespace algs {
   }
   
   
-  template<typename T>
-  struct LessCompare {
-    inline int operator()(const T& t1, const T& t2) const {
-      return t1 < t2 ? +1 : (t1 > t2 ? -1 : 0);
-    }
-  };
-  
-  template<typename T>
-  struct GreaterCompare {
-    inline int operator()(const T& t1, const T& t2) const {
-      return t1 > t2 ? 1 : (t1 < t2 ? -1 : 0); 
-    }
-  };
-  
-  template<typename T>
-  inline LessCompare<T> less() {
-    static LessCompare<T> cmp;
-    return cmp;
-  }
-  
-  template<typename T>
-  inline GreaterCompare<T> greater() {
-    static GreaterCompare<T> cmp;
-    return cmp;
-  }
   
 }
 }
