58b23a46724d7b6cff37f2313f3b03e382109557
Ziseon Jiao <ziseon.jiao@sap.com>
2017-02-15 17:43:54 +0800

    Add test

--- sort_example.cpp
+++ sort_example.cpp
@@ -1,14 +1,14 @@
 #include <vector>
 #include <iostream>
 
-#include "sorting/sort.h"
+#include "sorting/elementory_sort.h"
 
 int main() {
   using namespace emeng;
   using namespace std;
 
-  vector<int> arr{ 5, 4, 3, 2, 1 };
-  algs::selection_sort(arr, algs::less<int>());
+  vector<int> arr{ 5, 67, 3434, 2, 45, 672, 467, 945 };
+  algs::insertion_sort(arr, algs::greater<int>());
   for (const auto& item : arr) {
     cout << item << " ";
   }
