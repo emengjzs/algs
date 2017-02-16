8f8c5e785f5fa273c846f149ae8c7d07f78f2693
Ziseon Jiao <ziseon.jiao@sap.com>
2017-02-16 16:01:16 +0800

    Add three way quick sort

--- sorting/quick_sort.h
+++ sorting/quick_sort.h
@@ -41,6 +41,40 @@ int _partition(std::vector<T>& arr, const Compare<T>& cmp, int low, int high) {
   return j;
 }
 
+template <typename T, template <typename> class Compare>
+inline void three_way_quick_sort(std::vector<T>& arr, const Compare<T>& cmp) {
+  three_way_quick_sort(arr, cmp, 0, arr.size() - 1);
+}
+
+template <typename T, template <typename> class Compare>
+void three_way_quick_sort(std::vector<T>& arr, const Compare<T>& cmp, int low, int high) {
+  if (low < high) {
+    T& pivot = arr[low];
+    int ptr = low + 1;
+    int less_ptr = low + 1; // [low, less_ptr)
+    int greater_ptr = high; // (greater_ptr, high]
+    while (ptr <= greater_ptr) {
+      int r = cmp(arr[ptr], pivot);
+      if (r > 0) {
+        std::swap(arr[ptr], arr[greater_ptr]);
+        -- greater_ptr; 
+      }
+      else if (r < 0) {
+        std::swap(arr[ptr], arr[less_ptr]);
+        ++ less_ptr;
+        ++ ptr;
+      }
+      else {
+        ++ ptr;
+      }
+    }
+    std::swap(arr[low], arr[less_ptr - 1]);
+    three_way_quick_sort(arr, cmp, low, less_ptr - 1);
+    three_way_quick_sort(arr, cmp, greater_ptr + 1, high);
+    
+  }
+}
+
 
 }
 }
