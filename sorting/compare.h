0ee4afc16daabf179a21c55a913b91ea4f41d7c5
Ziseon Jiao <ziseon.jiao@sap.com>
2017-02-16 14:10:23 +0800

    Add partitialize int

--- sorting/compare.h
+++ sorting/compare.h
@@ -3,21 +3,35 @@
 namespace emeng {
 namespace algs {
   
-
   template <typename T>
   struct LessCompare {
     inline int operator()(const T& t1, const T& t2) const {
-      return t1 < t2 ? +1 : (t1 > t2 ? -1 : 0);
+      return t1 < t2 ? -1 : (t1 > t2 ? +1 : 0);
     }
   };
-
+  
+  template <>
+  struct LessCompare<int> {
+    inline int operator()(const int t1, const int t2) const {
+      return t1 - t2;
+    }
+  };
+  
   template <typename T>
   struct GreaterCompare {
     inline int operator()(const T& t1, const T& t2) const {
-      return t1 > t2 ? 1 : (t1 < t2 ? -1 : 0);
+      return t1 > t2 ? -1 : (t1 < t2 ? +1 : 0);
     }
   };
-
+  
+  template <>
+  struct GreaterCompare<int> {
+    inline int operator()(const int t1, const int t2) const {
+      return t2 - t1;
+    }
+  };
+  
+  
   template <typename T>
   inline LessCompare<T> less() {
     static LessCompare<T> cmp;
